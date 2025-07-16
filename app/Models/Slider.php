bb69a3d7f6828d087272a9f64c8f0a9283029fb1
Patrick Her <renfu.her@gmail.com>
2025-07-16 22:55:26 +0800

    新增 Slider 模型的可填充屬性及類型轉換設定，以支持資料的有效管理。

--- app/Models/Slider.php
+++ app/Models/Slider.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Slider extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'description',
+        'image',
+        'link',
+        'sort',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'sort' => 'integer',
+        'is_active' => 'boolean',
+    ];
 }
